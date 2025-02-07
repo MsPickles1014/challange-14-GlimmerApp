7bab7c0db886fe32aad89798a710df247564e2a9
Ms Pickles1014 <177317399+MsPickles1014@users.noreply.github.comgit config --global user.emailgit config --global user.email noeladnelson@gmail.com>
2025-02-06 21:42:53 -0500

    setup the base layout for favourite table and seed files

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,6 +1,12 @@
 import sequelize from '../config/connection.js';
 import { UserFactory } from './user.js';
+import { FavouriteFactory } from '';
 
 const User = UserFactory(sequelize);
+const Favourite = FavouriteFactory(sequelize);
 
-export { User };
+
+// // Define association: A Favorite belongs to a User
+// Favorite.belongsTo(User, { foreignKey: "userId", onDelete: "CASCADE" });
+
+export { User, Favourite};

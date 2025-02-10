ef9e322b61795255fc38e388d35896b359ad4289
katiemerrick <katiemerrickmarrketing@gmail.com>
2025-02-10 13:58:07 -0500

    Login component code

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,7 +4,7 @@ import { useState, useEffect, useLayoutEffect } from "react";
 import { retrieveUsers } from "../api/userAPI";
 import type { UserData } from "../interfaces/UserData";
 import ErrorPage from "./ErrorPage";
-import UserList from '../components/Users';
+import UserList from './Users';
 import auth from '../utils/auth';
 
 const Home = () => {

fc9fe00c08018a3fa15905f60713158c503faf4d
katiemerrick <katiemerrickmarrketing@gmail.com>
2025-02-10 14:51:55 -0500

    imported email and password icon

--- client/src/components/LoginSignup.jsx
+++ client/src/components/LoginSignup.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import './LoginSignup'
 
 import user_icon from '../assets/person.png'
+import email_icon from '../assets/email.png'
+import password_icon from '../assets/password.png'
 
 const LoginSignup = () => {
     return (
